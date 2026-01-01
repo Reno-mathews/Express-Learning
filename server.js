f24cc86fbf315bcda183c09d94be903f11819481
Reno-mathews <renomathews10pro@gmail.com>
2026-01-01 09:57:32 +0530

    Update

--- server.js
+++ server.js
@@ -5,6 +5,19 @@ const app = express();
 app.get("/", (req, res) => {
     res.send("Hello from Express!");
 });
+
+app.get("/api", (req,res) => {
+    res.json({ message: "Hello API "});
+});
+
+app.post("/tasks", (req, res) => {
+    const task = req.body.text;
+
+    res.jspm({
+        id: 1,
+        text: task
+    });
+});
 // start server
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
