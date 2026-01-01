c14575f855b5bf9fca7b6a1431bb04b7a42bb585
Reno-mathews <renomathews10pro@gmail.com>
2026-01-01 09:35:35 +0530

    Update

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("Hello from Express!");
+});
 // start server
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
-    
+
 })
