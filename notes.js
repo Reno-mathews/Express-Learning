5880f1978c3763a534a05ab52c1eba4c3e264de0
Reno-mathews <renomathews10pro@gmail.com>
2026-01-07 17:48:29 +0530

    Update

---
+++ notes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const Note = require("../models/Note");
+
+const router = express.Router();
+
+router.get("/", async (req,res) => {
+    const notes = await Note.find();
+    res.json(notes);
+});
+
+router.post("/", async (req,res) => {
+    const { title, content } = req.body;
+
+    const newNote = new Note({
+        title,
+        content,
+    });
+
+    const savedNote = await newNote.save();
+    res.status(201).json(savedNote);
+});
+
+module.exports = router;
