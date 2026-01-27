864a28552201b39cf3b0beeb7a8a11dd0530db9f
Reno-mathews <renomathews10pro@gmail.com>
2026-01-27 14:59:35 +0530

    Practicing

---
+++ .history/contollers/auth.controllers_20260127142752.js
@@ -0,0 +1,19 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateToken = (user) => {
+    return jwt.sign(
+        { userId: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "7d"}
+    );
+};
+
+// SIGNUP
+exports.signup = async (req, res) => {
+    const { email, password } = req.body;
+
+    if(!email || !password)
+        return res.status(400).json({ message: ""})
+}
