ffe8e1903d27bc413d6af64a2b31cbda56a374cf
Reno-mathews <renomathews10pro@gmail.com>
2026-01-27 16:38:47 +0530

    Practicing

---
+++ .history/contollers/task.controller_20260127163843.js
@@ -0,0 +1,35 @@
+const Task = require("../modles/Task");
+
+// GET tasks (user gewts own, admin gets all)
+exports.getTasks = async (req, res) => {
+    const filter =
+        req.user.role === "admin" ? {} : { owner: req.user.userId };
+
+        const tasks = await Task.find(filter);
+        res.json(tasks);
+};
+
+// CREATE task
+exports.createTask = async (req, res) => {
+    const { title, column } = req.body;
+
+    if (!title)
+        return res.status(400).json({ message: "Title is required" });
+    const task = await Task.create({
+        title,
+        column: column || "todo",
+        owner: req.user.userId,
+    });
+
+    res.status(201).json(task);
+};
+
+// UPDATE task (owner or admin)
+exports.updateTask = async (req, res) => {
+    const task = await Task.findById(req.params.id);
+    if (!task)
+        return res.status(404).json({ message: "Task not found" });
+
+    const isOwner = task,owner
+
+}
